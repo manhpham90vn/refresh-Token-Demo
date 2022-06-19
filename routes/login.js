1316d1747609b3ef3c3ccfc6f3d3bafbe82c0cb8
Manh Pham <manhpham90vn@icloud.com>
2022-06-19 19:05:31 +0700

    update

--- routes/login.js
+++ routes/login.js
@@ -5,20 +5,21 @@ var jwt = require('jsonwebtoken');
 var store = require('store');
 var users = require('../users');
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
     const postData = req.body;
     if (users.users.some(item => item.email === postData.email) && users.users.some(item => item.password == postData.password)) {
 
         const payload = { 'email': postData.email }
 
         const token = jwt.sign(payload, config.secret, {
-            expiresIn: config.tokenLife 
+            expiresIn: config.tokenLife
         })
 
         const refreshToken = jwt.sign(payload, config.refreshTokenSecret, {
             expiresIn: config.refreshTokenLife
         })
 
+        store.clearAll()
         store.set(refreshToken, token);
 
         const response = {
@@ -26,11 +27,11 @@ router.post('/', function(req, res) {
             "token": token,
             "refreshToken": refreshToken
         }
-        
+
         res.json(response)
     } else {
-        res.json({"status": "error", "message": "login error"})
+        res.status(403).json({ "status": "error", "message": "login error" })
     }
-  });
-  
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,16 @@ var config = require('../config')
 var users = require('../users');
 
 router.use(require('../middleware/checkToken'));
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 
   const token = req.headers.authorization.split(' ')[1]
   const email = jwt.verify(token, config.secret).email;
-  const response = users.users.find(data => data.email === email)
-  delete response.password
+  const user = users.users.find(data => data.email === email)
+  const response = {
+    "email": user.email,
+    "name": user.name,
+    "date": Date()
+  }
   res.json(response)
 });
 
