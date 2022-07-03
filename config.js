57d74ab23e9bf8ccdd084bda4e8d0d15830ed2af
Manh Pham (OFA iOS) <manhpham90vn@icloud.com>
2022-07-03 19:20:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     'secret': 'test-secret-123',
     'refreshTokenSecret': 'test-refresh-secret-123',
     'tokenLife': 60, // 1 minute
-    'refreshTokenLife': 60 * 3 // 2 minute
+    'refreshTokenLife': 60 * 3 // 3 minute
 }
