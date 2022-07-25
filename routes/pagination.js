a30d8bf29bbd5f541f63532de9cfaf4e3a9f474d
Manh Pham <manhpham90vn@icloud.com>
2022-07-25 17:26:36 +0700

    fix

--- routes/pagination.js
+++ routes/pagination.js
@@ -7,25 +7,27 @@ var users = require('../users');
 var randomName = require('node-random-name');
 
 router.use(require('../middleware/checkToken'));
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     var page = req.query.page
     var arrs = [];
-    var count = 50
+    var count = 60
     for (let i = 0; i < count; i++) {
         arrs.push({
-            "name": randomName(),
-            "age": Math.floor(Math.random() * 60) + 20
+            "id": i,
+            "name": randomName() + " " + i,
+            "age": Math.floor(Math.random() * 60) + 20,
+            "website": "https://github.com/manhpham90vn"
         })
     }
     if (page == 1) {
-        res.json({ array: arrs.splice(0, 19) });
+        res.json({ array: arrs.slice(0, 19) });
     } else if (page == 2) {
-        res.json({ array: arrs.splice(20, 39) });
+        res.json({ array: arrs.slice(20, 39) });
     } else if (page == 3) {
-        res.json({ array: arrs.splice(40, 50) });
+        res.json({ array: arrs.slice(40, 59) });
     } else {
         res.json({ array: [] });
     }
-  });
-  
+});
+
 module.exports = router;
